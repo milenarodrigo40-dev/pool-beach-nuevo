56b34b59a32bbd37c68a9b51e9e926434f1ee2ca
milenarodrigo40-dev <milenarodrigo40@gmail.com>
2026-09-03 18:39:48 -0300

    Crear supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://nmpakeovqkdajtswjpgm.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_FyuFdiW_2eXNScLd9MShng_rLxxztsu";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
